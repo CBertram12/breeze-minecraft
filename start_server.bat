0e4e90b54f93b232348c1370b99d63439be6e023
Cameron <cambo4321@gmail.com>
2021-10-12 11:32:19 -0400

    Cambo updated the server

---
+++ start_server.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enabledelayedexpansion
+echo "Running script..."
+echo.
+git pull origin master
+echo.
+set name=TEST
+if exist current_user (
+	for %%R in (current_user) do if not %%~zR lss 1 (
+		echo The server is currently in use by:
+		more current_user
+	) else (
+		set /P name="Enter ID: "
+		echo !name! > current_user
+		git add current_user
+		git commit -m "!name! started the server"
+		git push origin master
+		java -Xmx1024M -Xms1024M -jar .\server.jar nogui
+		echo > current_user
+		git add --all
+		git commit -m "!name! updated the server"
+		git push origin master
+	)
+) else (
+	echo "current_user does not exist"
+)
+echo.
+pause
